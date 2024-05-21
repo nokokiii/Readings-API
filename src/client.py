374e045d5470f01592a45aa1ec62bef3da722486
Mund3k <kubat1611@gmail.com>
2024-05-21 15:25:59 +0700

    some client changes

--- src/client.py
+++ src/client.py
@@ -1,13 +1,25 @@
 import requests
-import controllers as controllers
+from thefuzz import fuzz
 
 
-def fetch_data():
+def fetch_data(title: str):
+
     r = requests.get("https://wolnelektury.pl/api/books/").json()
+    books = r
+    last_ratio = 0
+    for book in books:
+        ratio = fuzz.ratio(title, book['title'])
+        if title == book['title'] or ratio > last_ratio:
+            last_ratio = ratio
+            found_book = {
+                "title": book['title'],
+                "author": book['author'],
+                "epoch": book['epoch'],
+                "genre": book['genre'],
+                "kind": book['kind'],
+            }
 
-    data = r
-
-    response = requests.post(url="http://127.0.0.1:5001/add_book", json=data,
+    response = requests.post(url="http://localhost:5001/books/add_book", json=r,
                              headers={"Content-Type": "application/json"})
 
     if response.status_code == 201:
@@ -15,5 +27,8 @@ def fetch_data():
     else:
         print("Failed to add book.")
         print(response.text)
-        
-    return data
+
+    return found_book
+
+
+print(fetch_data("Pantera"))
