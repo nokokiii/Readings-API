f0e9c1dbb8f02a7072c58431b4df1388be35094d
Nokokiii <kubanokoki@gmail.com>
2024-05-22 18:18:50 +0200

    fixing responses

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, Response, jsonify, request
 
-from src.app.logic import getLogic
+from src.app.logic import Logic
+from src.app.utils import get_status_code
 
 
 app = Flask(__name__)
-
-logic = getLogic()
+logic = Logic()
 
 
 @app.route('/')
@@ -16,39 +16,21 @@ def ping():
 def add_book():
     data = request.get_json()
     status_msg, res = logic.add_book(title=data['title'], author=data['author'], kind=data['kind'])
-    
-    if status_msg == "Error":
-        status = 500
-    elif status_msg == "Created":
-        status = 201
 
-    return Response(status=status, response=jsonify({"msg": res}))
+    return Response(status=get_status_code(status_msg=status_msg), response=jsonify(res))
 
  
 @app.get('/book')
 def get_book():
     title = request.params.get('title')
-
     status_msg, res = logic.get_book(title=title)
 
-    # Put this into function in somewhere
-    if status_msg == "Error":
-        status = 500
-    elif status_msg == "Not Found":
-        status = 404
-    elif status_msg == "OK":
-        status = 200
-
-    return Response(status=status, response=jsonify(res))
+    return Response(status=get_status_code(status_msg=status_msg), response=jsonify(res))
 
 
 @app.get('/books')
 def get_books():
-    authors = request.params.get('authors')
-    kinds = request.params.get('kinds')
-    # TODO: Add more filters
-
-    status_msg, res = logic.get_books(authors=authors, kinds=kinds)
+    status_msg, res = logic.get_books(params=request.params)
 
 
 if __name__ == "__main__":
