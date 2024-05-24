2deacb4b29f41d12dcc329d3ad317e1d6d80935f
Mund3k <kubat1611@gmail.com>
2024-05-25 04:01:37 +0700

    client for list of books

--- src/client.py
+++ src/client.py
@@ -3,6 +3,7 @@ import re
 import requests
 from thefuzz import fuzz
 
+
 def fetch_data() -> None:
     while True:
         print("1. Dodaj pojedyńczą książkę")
@@ -27,30 +28,36 @@ def fetch_data() -> None:
                         }
 
                 if found_book:
-                    print(f"Book {title} added")
-                    # response = requests.post(url="http://localhost:5001/books/add_book", json=found_book,
-                    #                         headers={"Content-Type": "application/json"})
+                    response = requests.post(url="http://localhost:5001/books/add_book", json=found_book,
+                                            headers={"Content-Type": "application/json"})
 
-                    # if response.status_code == 200:
-                    #     print("Book added succesfully")
-                    # else:
-                    #     print(f"Failed to add book:\n {response.text}")
+                    if response.status_code == 200:
+                        print(f"{found_book['title']} added succesfully")
+                    else:
+                        print(f"Failed to add book:\n {response.text}")
             case 2:
-                categories = [f"kinds/{cat}" for cat in (str(input("Podaj kategorie które/ą chcesz dodać: (format: kategoria kategoria)\n"))).split()]
-                author = [f"authors/{str(input("Podaj autora którego chcesz dodać: (format: imie nazwisko)\n")).lower().replace(' ', '-')}"]
+                categories = [f"kinds/{cat}" for cat in (
+                    input("Podaj kategorie które/ą chcesz dodać: (format: kategoria kategoria)\n")).strip().lower().split()]
+                author = [
+                    f"authors/{input("Podaj autora którego chcesz dodać: (format: imie nazwisko)\n").strip().lower().replace(' ', '-')}"]
 
                 response = requests.get(f"https://wolnelektury.pl/api/{'/'.join(categories + author)}/books")
+
                 if response.status_code != 200:
                     print(f"Error: {response.status_code}, {response.reason}")
-                else:
-                    print(response.json())
+                    break
+                list_of_books = [{"title": book["title"], "author": book["author"], "kind": book["kind"]} for book in response.json()]
+                for book in list_of_books:
+                    response = requests.post(url="http://localhost:5001/books/add_book", json=book,
+                                            headers={"Content-Type": "application/json"})
 
+                    if response.status_code == 200:
+                        print(f"{book['title']} added succesfully")
+                    else:
+                        print(f"Failed to add book:\n {response.text}")
             case 3:
                 print("Thanks for using our client.")
-                break 
+                break
 
-
-
-                
 
 fetch_data()
